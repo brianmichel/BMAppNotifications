e25a132f8d9a54db1a2e848bfd3894bf0551dda2
Brian Michel <brian.michel@gmail.com>
2012-11-02 11:03:22 -0400

    Initial Commit

---
+++ MLNotifications/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MLNotifications
+//
+//  Created by Brian Michel on 11/2/12.
+//  Copyright (c) 2012 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MLNotifications/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MLNotifications
+//
+//  Created by Brian Michel on 11/2/12.
+//  Copyright (c) 2012 Foureyes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
